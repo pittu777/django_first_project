ef55cf0a20ccbf73507b777ed800204dffd0dbdf
pittu777 <pittuprasanth98@gmail.com>
2026-06-14 13:12:29 +0530

    "changed home to students"

--- students/views.py
+++ students/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from .models import Student
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, "home.html")
 
 
 def student_list(request):
