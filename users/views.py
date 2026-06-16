ba39f31b51e4edfa71a4e55b9293b3101e7af01a
pittu777 <pittuprasanth98@gmail.com>
2026-06-16 16:37:31 +0530

    "added register form"

--- users/views.py
+++ users/views.py
@@ -1,12 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 
 def users(request):
-    return render(request, "users.html")
+    users = User.objects.all()
+    return render(request, "users.html", {"users": users})
 
 
 def register(request):
-    return render(request, "auth/register.html")
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(request, f"Account created successfully for {username}!")
+            return redirect("users:users_list")
+    else:
+        form = UserCreationForm()
+
+    return render(request, "auth/register.html", {"form": form})
