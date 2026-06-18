e1d33fe6db98c8d37a41bb5d839f5685765f6770
pittu777 <pittuprasanth98@gmail.com>
2026-06-18 10:54:41 +0530

    "created first api"

--- api/views.py
+++ api/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from students.models import Student
+from .serializers import StudentSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 
 
+@api_view(["GET"])
 def studentsView(request):
-    students = {"id": 1, "name": "pittu"}
-    return JsonResponse(students)
+    if request.method == "GET":
+        students = Student.objects.all()
+        serializer = StudentSerializer(students, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
